2e003aabe671dce12d01f69f686a7b1d17f61e92
Marcelo Augusto <nextlevelcode014@gmail.com>
2025-04-06 10:54:45 -0300

    binary search

---
+++ binary_search/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int binary_search(int arr[], int n, int len) {
+  int l = 0;
+  int r = len - 1;
+  int steps = 0;
+
+  while (l <= r) {
+    int mid = l + (r - l) / 2;
+    steps++;
+    if (arr[mid] == n) {
+      printf("steps: %d\n", steps);
+      return mid;
+    } else if (arr[mid] < n) {
+      l = mid + 1;
+    } else {
+      r = mid - 1;
+    }
+  }
+
+  return -1;
+}
+
+int main(void) {
+  int arr[] = {1,2,3,4,5,6,7,8,9};
+  int len = sizeof(arr) / sizeof(arr[0]);
+
+  int result = binary_search(arr, 9, len);
+  printf("result: %d", result);
+
+  return 0;
+}
---
+++ binary_search/main.rs
@@ -0,0 +1,29 @@
+fn binary_search(arr: &[i32], target: i32) -> Option<usize> {
+    let mut left = 0;
+    let mut right = arr.len().wrapping_sub(1);
+    let mut steps = 0;
+
+    while left <= right {
+        steps += 1;
+        let mid = left + (right - left) / 2;
+
+        if arr[mid] == target {
+            println!("steps: {}", steps);
+            return Some(mid);
+        } else if arr[mid] < target {
+            left = mid + 1;
+        } else {
+            right = mid.wrapping_sub(1);
+        }
+    }
+
+    None
+}
+
+fn main() {
+    let arr = [1,2,3,4,5,6,7,8,9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
+
+                        //   0 1 2 3 4 5 6 7 8
+    let result = binary_search(&arr, 6);
+    println!("{}", result.unwrap());
+}
