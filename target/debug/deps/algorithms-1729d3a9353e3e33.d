2338741bd8d0fa76dc9f6b5f58ad8ac3a49aaf0c
Marcelo Augusto <nextlevelcode014@gmail.com>
2025-06-13 19:48:11 -0300

    Hi

--- target/debug/deps/algorithms-1729d3a9353e3e33.d
+++
@@ -1,9 +0,0 @@
-/home/nextlevelcode/dev/algorithms/target/debug/deps/libalgorithms-1729d3a9353e3e33.rmeta: src/main.rs Cargo.toml
-
-/home/nextlevelcode/dev/algorithms/target/debug/deps/algorithms-1729d3a9353e3e33.d: src/main.rs Cargo.toml
-
-src/main.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
-# env-dep:CLIPPY_CONF_DIR
